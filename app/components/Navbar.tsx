d13b9462ad4448bdf5254d772a042d006ee3c323
Gama <gamaliel.g.g131@gmail.com>
2025-04-16 12:55:34 -0600

    Add nav bar

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import Link from "next/link";
+
+export default function Navbar() {
+  return (
+    <nav className="fixed top-0 left-0 right-0 w-full py-4 px-6 border-b border-gray-100 shadow-sm bg-white z-10">
+      <div className="max-w-7xl mx-auto flex justify-between items-center">
+        <Link
+          href="/"
+          className="text-xl font-medium text-gray-800 hover:text-gray-600 transition"
+        >
+          post-it
+        </Link>
+      </div>
+    </nav>
+  );
+}
